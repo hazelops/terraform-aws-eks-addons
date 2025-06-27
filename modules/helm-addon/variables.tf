36b11adc0e795e99f6f93775a1b77930fe8a9bf9
Dmitry Borisov <dmitry.borisov@hazelops.com>
2025-06-27 12:53:26 +0300

    Some cleanup is done

--- modules/helm-addon/variables.tf
+++ modules/helm-addon/variables.tf
@@ -23,19 +23,6 @@ variable "set_sensitive_values" {
   default = []
 }
 
-
-# variable "set_values" {
-#   description = "Forced set values"
-#   type        = any
-#   default     = []
-# }
-#
-# variable "set_sensitive_values" {
-#   description = "Forced set_sensitive values"
-#   type        = any
-#   default     = []
-# }
-
 variable "manage_via_gitops" {
   description = "Determines if the add-on should be managed via GitOps"
   type        = bool
